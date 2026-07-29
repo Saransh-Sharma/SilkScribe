dfa4ceeb10332b9dabb112e67eeecbab3f49b330
Saransh <saransh1337@gmail.com>
2026-07-29 15:51:23 +0530

    chore(deps): add Manrope font support

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "@fontsource/manrope/latin-400.css";
+import "@fontsource/manrope/latin-500.css";
+import "@fontsource/manrope/latin-600.css";
+import "@fontsource/manrope/latin-700.css";
 import App from "./App";
 import "./theme.css";
 
